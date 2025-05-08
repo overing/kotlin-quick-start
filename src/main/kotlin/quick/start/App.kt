2203047c86ca4a5e8ec03628459314fd79b0bb05
chunyi.chang <chunyi.chang@smartdaily.com.tw>
2025-05-08 21:39:58 +0800

    feat: 進入點風格

--- src/main/kotlin/quick/start/App.kt
+++ src/main/kotlin/quick/start/App.kt
@@ -1,8 +1,8 @@
 package quick.start
 
+import io.ktor.server.application.Application
 import io.ktor.server.application.call
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.netty.EngineMain;
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -11,24 +11,23 @@ import org.kodein.di.DI
 import org.kodein.di.instance
 
 class MyService {
-    val greeting: String
-        get() = "Hello world."
+    val greeting: String get() = "Hello world."
 }
 
-fun main() {
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
     val di = DI {
         bindSingleton<MyService> { MyService() }
     }
-
-    embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Is Working!")
-            }
-            get("/api/hello") {
-                val service: MyService by di.instance()
-                call.respondText(service.greeting)
-            }
+    
+    routing {
+        get("/") {
+            call.respondText("Is Working!")
         }
-    }.start(wait = true)
+        get("/api/hello") {
+            val service: MyService by di.instance()
+            call.respondText(service.greeting)
+        }
+    }
 }
